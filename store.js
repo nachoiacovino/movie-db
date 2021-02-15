19c84030c51008c93c17950eab3ddeb2229e7aee
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-02-15 18:12:36 +0100

    Add redux-persist and redux-logger

--- store.js
+++ store.js
@@ -1,5 +1,8 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducer';
@@ -8,14 +11,19 @@ import rootSaga from './saga';
 const bindMiddleware = (middleware) => {
   if (process.env.NODE_ENV !== 'production') {
     const { composeWithDevTools } = require('redux-devtools-extension');
-    return composeWithDevTools(applyMiddleware(...middleware));
+    const logger = createLogger();
+    return composeWithDevTools(applyMiddleware(logger, ...middleware));
   }
   return applyMiddleware(...middleware);
 };
 
+const persistConfig = { key: 'root', storage, whitelist: ['user'] };
+
+const reducer = persistReducer(persistConfig, rootReducer);
+
 export const makeStore = (context) => {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(rootReducer, bindMiddleware([sagaMiddleware]));
+  const store = createStore(reducer, bindMiddleware([sagaMiddleware]));
 
   store.sagaTask = sagaMiddleware.run(rootSaga);
 
