897b9b03f1199ef1371edf01129ec030db8c381b
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-02-16 11:24:17 +0100

    Render playlists

--- pages/playlists.js
+++ pages/playlists.js
@@ -7,11 +7,20 @@ const playlists = () => {
   const playlists = useSelector((state) => state.playlists);
 
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4 flex flex-col md:flex-row'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4'>
       <h1 className='text-3xl font-semibold'>My playlists</h1>
       <button onClick={() => dispatch(addPlaylist('test'))}>
         Add playlist
       </button>
+      <div>
+        {playlists.length > 0 && (
+          <ul>
+            {playlists.map((playlist) => (
+              <li>{playlist.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
