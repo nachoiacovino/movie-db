2851be16fe713c986129507af68d5b54ff381bd2
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-02-16 13:56:42 +0100

    Modify landing page

--- components/Search.js
+++ components/Search.js
@@ -17,7 +17,7 @@ const Search = () => {
         <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
           {/* Heroicon name: solid/search */}
           <svg
-            className='h-5 w-5 text-gray-400'
+            className='h-7 w-7 text-gray-400'
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 20 20'
             fill='currentColor'
@@ -34,7 +34,7 @@ const Search = () => {
           id='search'
           name='search'
           onChange={onSearchChange}
-          className='block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-black focus:border-black sm:text-sm'
+          className='block w-full pl-12 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-black focus:border-black text-2xl'
           placeholder='Search'
           type='search'
         />
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { searchData } from '../actions';
 import Card from '../components/Card';
 import Search from '../components/Search';
 
-/* import { END } from 'redux-saga';
-import { wrapper } from '../store'; */
-
 const Index = () => {
   const searchResults = useSelector((state) => state.searchResults);
-  console.log(searchResults);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(searchData('fast'));
+  }, [dispatch]);
 
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-      <div className='flex justify-center mt-6'>
+      <div className='flex justify-center mt-6 flex-col items-center space-y-5 text-center'>
+        <h1 className='text-4xl font-bold'>Find you new favorite show</h1>
         <Search />
       </div>
-      <div className='grid max-w-lg gap-5 mx-auto mt-12 md:grid-cols-3 lg:grid-cols-5 lg:max-w-none'>
+      <div className='grid max-w-lg gap-5 mx-auto mt-8 md:grid-cols-3 lg:grid-cols-5 lg:max-w-none mb-6'>
         {searchResults?.map((show) => (
           <Card key={show.imdbID} show={show} />
         ))}
@@ -24,14 +27,5 @@ const Index = () => {
     </div>
   );
 };
-
-/* export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-  if (!store.getState()?.searchResults) {
-    store.dispatch(searchData(''));
-    store.dispatch(END);
-  }
-
-  await store.sagaTask.toPromise();
-}); */
 
 export default Index;
